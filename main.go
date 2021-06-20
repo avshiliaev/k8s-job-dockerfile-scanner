fdb2ed4ff2bc2163af6b234d7abe885ae453cd62
Artem <a.v.shiliaev@gmail.com>
2021-06-20 13:28:28 +0200

    basic cli interface

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+)
 
+func main() {
+	app := &cli.App{
+		Name:  "Scanner",
+		Usage: "scans repositories for Dockerfiles and retrieves image information",
+		Action: func(c *cli.Context) error {
+			fmt.Println("Result")
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
