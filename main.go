7acac6948fd6fb4acb5915cfc695650bca8061c2
Artem <a.v.shiliaev@gmail.com>
2021-06-20 14:00:32 +0200

    cli flags and logic

--- main.go
+++ main.go
@@ -1,34 +1,59 @@
 package main
 
 import (
-	"fmt"
 	"github.com/urfave/cli/v2"
 	"log"
+	"net/http"
 	"os"
+	"redhat-sre-task-dockerfile-scanner/src/api/github"
+	"redhat-sre-task-dockerfile-scanner/src/parsers"
+	"redhat-sre-task-dockerfile-scanner/src/readers"
+	scanners "redhat-sre-task-dockerfile-scanner/src/scanner"
+	"redhat-sre-task-dockerfile-scanner/src/validators"
+	"redhat-sre-task-dockerfile-scanner/src/writers"
 )
 
 func main() {
 
-	// ./scanner -input http://github.com/ -pattern Dockerfile
+	// ./scanner -i http://github.com/ -p Dockerfile -v github -o json
 	app := &cli.App{
 		Name:  "Scanner",
 		Usage: "scans repositories for Dockerfiles and retrieves image information",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:  "input",
-				Value: "",
-				Usage: "link to a txt input file",
+				Name:     "input",
+				Aliases:  []string{"i"},
+				Value:    "",
+				Usage:    "link to a txt input file",
+				Required: true,
 			},
 			&cli.StringFlag{
-				Name:  "pattern",
-				Value: "Dockerfile",
-				Usage: "a filename pattern to match",
+				Name:    "format",
+				Aliases: []string{"f"},
+				Value:   "txt",
+				Usage:   "input format",
+			},
+			&cli.StringFlag{
+				Name:    "pattern",
+				Aliases: []string{"p"},
+				Value:   "Dockerfile",
+				Usage:   "a filename pattern to match",
+			},
+			&cli.StringFlag{
+				Name:    "vendor",
+				Aliases: []string{"v"},
+				Value:   "github",
+				Usage:   "a repository vendor name",
+			},
+			&cli.StringFlag{
+				Name:    "out",
+				Aliases: []string{"o"},
+				Value:   "json",
+				Usage:   "an output format",
 			},
 		},
 		Action: func(c *cli.Context) error {
-			fmt.Println(c.String("input"))
-			fmt.Println(c.String("pattern"))
-			return nil
+			return RunFromContext(c)
 		},
 	}
 
@@ -36,4 +61,29 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func RunFromContext(c *cli.Context) error {
+	inputLink := c.String("input")
+	inputFormat := c.String("format")
+	repoVendor := c.String("vendor")
+	fileNamePattern := c.String("pattern")
+	outputFormat := c.String("out")
+
+	scanner := scanners.DockerFileScanner(inputLink)
+	if inputFormat == "txt" {
+		scanner.Read(readers.RemoteTxtReader(&http.Client{}))
+	}
+	if repoVendor == "github" {
+		scanner.Validate(validators.GitHubValidator())
+		scanner.Query(github.Api(github.GoogleGitHubClient()))
+	}
+	if fileNamePattern == "Dockerfile" {
+		scanner.Parse(parsers.DockerFileParser())
+	}
+	if outputFormat == "json" {
+		scanner.Write(writers.JsonStdWriter())
+	}
+
+	return nil
 }
